9ffe0ea3dc516f36f64f38dbe6308455de40c265
kr45732 <52721908+kr45732@users.noreply.github.com>
2025-02-04 16:24:22 -0500

    Begin user auth

--- moviesstore/urls.py
+++ moviesstore/urls.py
@@ -22,5 +22,6 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('movies/', include('movies.urls')),
+    path('accounts/', include('accounts.urls')),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
