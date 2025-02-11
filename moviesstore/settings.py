896b57510c9eb8b29c0cd8029063b24f959b7a85
Kevin Chen <kchen2461@gmail.com>
2025-02-11 16:44:55 -0500

    Add cart functionality and styling

--- moviesstore/settings.py
+++ moviesstore/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'movies',
     'home',
+    'cart'
 ]
 
 MIDDLEWARE = [
--- moviesstore/urls.py
+++ moviesstore/urls.py
@@ -23,5 +23,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('movies/', include('movies.urls')),
     path('', include('home.urls')),
+    path('cart/', include('cart.urls')),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
